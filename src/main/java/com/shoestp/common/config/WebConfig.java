e504853949b95301e72b4650743f1c68a38435d9
lingjian <251788868@qq.com>
2019-08-27 22:03:42 +0800

    嘎嘎嘎

---
+++ src/main/java/com/shoestp/common/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.shoestp.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 第三方拦截器的配置
+ */
+@Configuration
+public class WebConfig {
+
+//    @Bean
+//    public FilterRegistrationBean ThirdPartyFilter() {
+//        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+//
+//        ThirdPartyFilter thirdPartyFilter = new ThirdPartyFilter();
+//        registrationBean.setFilter(thirdPartyFilter);
+//
+//        List<String> urls = new ArrayList<>();
+//        urls.add("/*");
+//        registrationBean.setUrlPatterns(urls);
+//
+//        return registrationBean;
+//    }
+}
