791b50a9e40d4e3de2593e6a8626a3d37f3d910a
lingjian <251788868@qq.com>
2019-09-06 16:31:41 +0800

    返回历史分支

--- src/main/java/com/shoestp/test/vo/TestView.java
+++ src/main/java/com/shoestp/test/vo/TestView.java
@@ -12,15 +12,15 @@ import lombok.Data;
  * @since 2019-07-30 16:28:39
  */
 @Data
-@ApiModel(value = "TestVO",description = "test实体类对应的VO前端展示类")
+@ApiModel(value = "TestVO", description = "test实体类对应的VO前端展示类")
 public class TestView {
 
-    @ApiModelProperty(value="id",name="id",dataType = "int",example = "14")
-    private Integer id;
+  @ApiModelProperty(value = "id", name = "id", dataType = "int", example = "14")
+  private Integer id;
 
-    @ApiModelProperty(value="名称",name="name",dataType = "String",example = "张三")
-    private String name;
+  @ApiModelProperty(value = "名称", name = "name", dataType = "String", example = "张三")
+  private String name;
 
-    @ApiModelProperty(value="性别",name = "gender",dataType = "String",example = "男")
-    private String gender;
+  @ApiModelProperty(value = "性别", name = "gender", dataType = "String", example = "男")
+  private String gender;
 }
